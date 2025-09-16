a4ab501f9da4fe5d5ff72fc1daec28294ea83a62
Jeffrey Ruan <sparrowking002@gmail.com>
2025-09-16 18:35:37 +0000

    Change Theme

---
+++ .vitepress/config.ts
@@ -0,0 +1,62 @@
+import { defineConfig } from 'vitepress'
+import { getPosts } from './theme/serverUtils'
+
+//每页的文章数量
+const pageSize = 10
+
+const isProd = process.env.NODE_ENV === 'production'
+
+export default defineConfig({
+    title: 'sweetbrulee',
+    base: '/',
+    cacheDir: './node_modules/vitepress_cache',
+    description: 'vitepress,blog,blog-theme',
+    ignoreDeadLinks: true,
+    themeConfig: {
+        posts: await getPosts(pageSize),
+        website: 'https://github.com/airene/vitepress-blog-pure', //copyright link
+        // 评论的仓库地址 https://giscus.app/ 请按照这个官方初始化后覆盖
+        comment: {
+            repo: 'airene/vitepress-blog-pure',
+            repoId: 'MDEwOlJlcG9zaXRvcnkzODIyMjY5Nzg',
+            categoryId: 'DIC_kwDOFshSIs4CpZga'
+        },
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Category', link: '/pages/category' },
+            { text: 'Archives', link: '/pages/archives' },
+            { text: 'Tags', link: '/pages/tags' },
+            { text: 'About', link: '/pages/about' }
+            // { text: 'Airene', link: 'http://airene.net' }  -- External link test
+        ],
+        search: {
+            provider: 'local'
+        },
+        //outline:[2,3],
+        outline: {
+            label: '文章摘要'
+        },
+        socialLinks: [{ icon: 'github', link: 'https://github.com/airene/vitepress-blog-pure' }]
+    } as any,
+
+    srcExclude: isProd
+        ? [
+              '**/trash/**/*.md', // 排除所有 trash 目录
+              '**/draft/**/*.md', // 递归排除子目录
+              '**/private-notes/*.md', // 排除特定文件
+              'README.md'
+          ]
+        : ['README.md'],
+    vite: {
+        //build: { minify: false }
+        server: { port: 5000 }
+    },
+    markdown: {
+        math: true
+    }
+    /*
+      optimizeDeps: {
+          keepNames: true
+      }
+      */
+})
